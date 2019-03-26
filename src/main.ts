3e8ba7b33778ad8e6224419639f7a088888f14dd
ljl@nssp.biz <ljl@nssp.biz>
2019-03-26 18:56:32 +0800

    添加全局样式，引入vuex

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import Vue from 'vue';
 import App from '@/App';
 import router from '@/router';
-import store from './store';
+import store from '@/store';
 
+import './style/global.less';
 Vue.config.productionTip = false;
 
 new Vue({
-  // router,
+  router,
   store,
   render: h => h(App),
 }).$mount('#app');
