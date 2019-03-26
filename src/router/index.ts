7ff20b06c3e13f9238b28172ba277a5699d4ea8d
ljl@nssp.biz <ljl@nssp.biz>
2019-03-26 18:59:34 +0800

    添加路由规则

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,44 @@
 import Vue from 'vue';
-import Router from 'vue-router';
+import Router, {RouterOptions} from 'vue-router';
+import { routerItem } from '@/interface';
 
 Vue.use(Router);
 
-export const constantRouterMap = [
+export const constantRouterMap: routerItem[] & RouterOptions['routes'] = [
     {path: '/', redirect: '/home'}
 ]
+
+
+export const asyncRouterMap: routerItem[] = [
+    {
+        path: '/staff',
+        icon: 'user',
+        name: '员工操作',
+        component: () => import('../views/staff'),
+        meta: { key: 'staff' },
+        children: [
+            {
+                path: 'add',
+                icon: 'add',
+                name: '新增员工',
+                component: () => import('@/views/staff/add'),
+                meta: {key: 'staffadd'}
+            }
+        ]
+
+    }
+]
+
+
+
+
+
+
+
+
+
+
+
 
 
 export default new Router({
