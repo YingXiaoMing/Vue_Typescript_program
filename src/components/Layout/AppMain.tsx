c7bc46e898578ea82a71b27ed700e0309890da4d
ljl@nssp.biz <ljl@nssp.biz>
2019-03-27 18:26:55 +0800

    优化底层结构

--- src/components/Layout/AppMain.tsx
+++ src/components/Layout/AppMain.tsx
@@ -1,20 +1,31 @@
 
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue, Watch } from 'vue-property-decorator';
 import Header from './Header';
+import { Tabs } from 'ant-design-vue';
 import Sidebar from './Sidebar';
 import './AppMain.less';
 @Component({
-    components: {}
+    components: {
+        'a-tabs': Tabs,
+        'a-tab-pane': Tabs.TabPane,
+    }
 })
 export default class AppMain extends Vue {
+
+    @Watch('$route', { immediate: true, deep: true })
+    routeChange(to: any, from: any) {
+        this.$store.dispatch('AddTabPane' , to.path);
+    }
+
     render() {
+        const { sidebar: {opened} } = this.$store.state.app;
         return (
-            <div class='app-main'>
+            <div class={`app-main ${opened ? '' : 'sideLayout'}`}>
                 <Sidebar/>
                 <div class='page-content'>
                     <Header/>
                     <div class='page-wrap'>
-                        {/* <router-view></router-view> */}
+                        <router-view></router-view>
                     </div>
                 </div>
             </div>
