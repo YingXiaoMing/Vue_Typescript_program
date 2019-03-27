7fb60dbdaa3ab4b0ea8d4c26906b56d5bf059925
ljl@nssp.biz <ljl@nssp.biz>
2019-03-27 18:26:24 +0800

    添加TOGGLE_SIDEBAR和TAB_CHANGE方法

--- src/store/modules/app.ts
+++ src/store/modules/app.ts
@@ -7,17 +7,34 @@ const app = {
             hidden: true,
             icon: 'user',
             children: [],
-        }]
+        }],
+        sidebar: {
+            opened: true,
+        },
+        tabList: []
+
     },
     mutations: {
         [types.SET_MENUDATA]: (state: any, menuData: routerItem[]) => {
             state.menuData = menuData;
-        }
+        },
+        [types.TOGGLE_SIDEBAR]: (state: any) => {
+            state.sidebar.opened = !state.sidebar.opened;
+        },
+        [types.TAB_CHANGE]: (state: any, tabList: any) => {
+            state.tabList = tabList;
+        },
     },
     actions: {
         GetMenuData: (context: any, menuData: routerItem[]) => {
             context.commit(types.SET_MENUDATA, menuData);
-        }
+        },
+        ToggleSideBar: (context: any) => {
+            context.commit(types.TOGGLE_SIDEBAR);
+        },
+        AddTabPane: (context: any, url: string) => new Promise((resolve, rejct) => {
+            resolve(true);
+        })
     }
 }
 
