31f7cbd7964b4c708500fa34d25f8a72cbb5e9f4
ljl@nssp.biz <ljl@nssp.biz>
2019-03-27 18:25:13 +0800

    新增路由转成数组的方法

---
+++ src/utils/index.ts
@@ -0,0 +1,23 @@
+export function routeToArray(route: string): { routeArr: string[], params: string } {
+    if (!route) {
+        return {
+            routeArr: [],
+            params: '',
+        };
+    }
+    const arr: string[] = route.split('/');
+    const ret: string[] = [];
+    let params = '';
+    arr.shift();
+    arr.forEach((item, index) => {
+        if (parseInt(item, 10)) {
+            params = item;
+            return;
+        }
+        ret.push(index ? item : `/${item}`);
+    });
+    return {
+        routeArr: ret,
+        params,
+    };
+}  
