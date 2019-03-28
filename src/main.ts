3817463f5f480c093eb9f9e6e4f07bc92e287c5b
ljl@nssp.biz <ljl@nssp.biz>
2019-03-28 17:29:44 +0800

    添加路由拦截

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
 import Vue from 'vue';
 import App from '@/App';
-import router from '@/router';
+import router ,{asyncRouterMap} from '@/router';
 import store from '@/store';
-
 import './style/global.less';
+import { asyncRouterMap } from './router';
 Vue.config.productionTip = false;
+
+
+router.beforeEach((to, from, next) => {
+  if (!store.state.app.menuData.length) {
+    store.dispatch('GetMenuData',asyncRouterMap);
+    next();
+  }
+  next();
+})
+
+
+
+
+
 
 new Vue({
   router,
