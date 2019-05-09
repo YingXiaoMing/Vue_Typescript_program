eba7d094c0b7ceaba72e36e3f04dbaa68d64f615
ljl@nssp.biz <ljl@nssp.biz>
2019-05-09 09:01:45 +0800

    添加调离职记录查询

---
+++ src/views/staff/record/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>i want something to like this</div>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+import { Emit, Prop, Watch } from 'vue-property-decorator';
+export default class Record extends Vue {}
+</script>
+
+    
