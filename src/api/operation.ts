5f012d45d47ebd8fa004884a345abee84fdd8906
ljl@nssp.biz <ljl@nssp.biz>
2019-05-20 15:55:19 +0800

    添加组织架构操作的接口

--- src/api/operation.ts
+++ src/api/operation.ts
@@ -20,3 +20,51 @@ export const getEmployeeDismissedPosition = (employeeId: string) =>
 // 查看员工调离职的记录
 export const getEmployeeModificationRecord = (employeeId: string) =>
     request.get('/employee/' + employeeId + '/EmployeePositionModificationRecord');
+
+// 查询公司的组织架构
+export const getCompanyOrganizationChart = () =>
+    request.get('/CompanyOrganizationChart');
+
+// 编辑公司
+export const editCompanyData = (id: string, param: any) =>
+    request.patch('/company/' + id, param);
+
+// 编辑部门
+export const editDepartmentData = (companyId: string, id: string, param: any) =>
+    request.patch('/company/' + companyId + '/departments/' + id, param);
+
+// 新增公司
+export const newCompany = (param: any) =>
+    request.post('/company', param);
+
+// 新增部门
+export const newDepartment = (companyId: string, param: any) =>
+    request.post('/company/' + companyId + '/departments', param);
+
+// 新增顶级部门
+export const newHeadDeepartment = (companyId: string, param: any) =>
+    request.post('/company/' + companyId + '/HeadDepartment', param);
+
+// 删除公司
+export const deleteCompany = (companyId: string) =>
+    request.delete('/company/' + companyId);
+
+// 删除部门
+export const deleteDepartment = (companyId: string, departmentId: string) =>
+    request.delete('/company/' + companyId + '/departments/' + departmentId);
+
+// 编辑职位
+export const editPosition = (companyId: string, departmentId: string, id: string, param: any) =>
+    request.patch('/company/' + companyId + '/department/' + departmentId + '/Position/' + id, param);
+
+// 获取职位信息
+export const getPosition = (companyId: string, departmentId: string) =>
+    request.get('/company/' + companyId + '/department/' + departmentId + '/Position');
+
+// 删除职位
+export const deletePosition = (companyId: string, departmentId: string, id: string) =>
+    request.delete('/company/' + companyId + '/department/' + departmentId + '/Position/' + id);
+
+// 新增职位
+export const newPostion = (companyId: string, departmentId: string, param: any) =>
+    request.post('/company/' + companyId + '/department/' + departmentId + '/Position', param);
