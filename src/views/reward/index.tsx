266e9e11c2974218469e7e3aa0f85c21bba6f792
ljl@nssp.biz <ljl@nssp.biz>
2019-05-20 16:15:48 +0800

    添加奖惩的路由

---
+++ src/views/reward/index.tsx
@@ -0,0 +1,17 @@
+import { Component , Vue } from 'vue-property-decorator';
+
+@Component({
+    name: 'reward',
+})
+
+export default class Staff extends Vue {
+    private $store: any;
+    public render() {
+        const { keepList } = this.$store.state.app;
+        return (
+            <keep-alive include={keepList}>
+                <router-view/>
+            </keep-alive>
+        );
+    }
+}
