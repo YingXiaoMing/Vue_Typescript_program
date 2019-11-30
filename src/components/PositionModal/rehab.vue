a5a7a1b63d4bf50d1bd6072de50a897d744db551
ljl@nssp.biz <ljl@nssp.biz>
2019-11-30 10:48:21 +0800

    添加工单号和复职类型

--- src/components/PositionModal/rehab.vue
+++ src/components/PositionModal/rehab.vue
@@ -1,6 +1,13 @@
 <template>
     <a-form :form="form">
         <a-row>
+            <a-col :span="12">
+                <a-form-item label="工单号" v-bind="formItemLayout">
+                    <a-input disabled v-decorator="['orderNum', {initialValue: data.orderNum}]"></a-input>
+                </a-form-item>
+            </a-col>
+        </a-row>
+        <a-row>
            <a-col :span="12">
                 <a-form-item label="员工工号" v-bind="formItemLayout">
                     <a-input disabled v-decorator="['num', {initialValue: data.num}]"></a-input>
@@ -15,24 +22,36 @@
         <a-divider>复职操作</a-divider>
         <a-row>
             <a-col :span="18">
+                <a-form-item label="复职类型" v-bind="formItemLayout2">
+                    <a-select v-if="data.isEdit"  v-decorator="['employeePositionChangeTypeId', {initialValue: typeOption[0].key, rules: [{ required: true, message: ' ' }]}]">
+                        <a-select-option v-for="item in typeOption" :value="item.key">{{item.label}}</a-select-option>
+                    </a-select>
+                    <a-input v-else disabled v-decorator="['employeePositionChangeTypeName', {initialValue: data.employeePositionChangeTypeName}]"></a-input>
+                </a-form-item>
+            </a-col>
+        </a-row>
+        <a-row>
+            <a-col :span="18">
                 <a-form-item label="复职职位" v-bind="formItemLayout2">
-                    <a-select v-decorator="['reinstatedPositionId', {initialValue: rehabPositionOption[0].key, rules: [{ required: true, message: ' ' }]}]">
+                    <a-select  v-if="data.isEdit" v-decorator="['reinstatedPositionId', {initialValue: data.positionId, rules: [{ required: true, message: ' ' }]}]">
                         <a-select-option v-for="item in rehabPositionOption" :value="item.key">{{item.label}}</a-select-option>
                     </a-select>
+                    <a-input disabled v-else v-decorator="['positionName',{initialValue: data.positionName}]"></a-input>
                 </a-form-item>
             </a-col>
         </a-row>
         <a-row>
             <a-col :span="18">
                 <a-form-item label="生效日期" v-bind="formItemLayout2">
-                    <a-date-picker v-decorator="['effectiveDate', {initialValue: momentFromDate(data.effectiveDate), rules: [{ required: true, message: ' ' }] }]"></a-date-picker>
+                    <a-date-picker v-if="data.isEdit" v-decorator="['effectiveDate', {initialValue: momentFromDate(data.effectiveDate), rules: [{ required: true, message: ' ' }] }]"></a-date-picker>
+                    <a-input v-else disabled v-decorator="['effectiveDate', {initialValue: data.effectiveDate }]"></a-input>  
                 </a-form-item>
             </a-col>
         </a-row>
         <a-row>
             <a-col :span="24">
                 <a-form-item label="复职原因" v-bind="formItemLayout3">
-                    <a-textarea v-decorator="['reason', {initialValue: data.reason}]" rows="4"></a-textarea>
+                    <a-textarea :disabled="!data.isEdit" v-decorator="['reason', {initialValue: data.reason}]" rows="4"></a-textarea>
                 </a-form-item>
             </a-col>
         </a-row>
@@ -40,8 +59,8 @@
 </template>
 <script lang="ts">
 import { Component, Vue, Prop } from 'vue-property-decorator';
-import { SelectValues } from '@/interface';
-import { getEmployeePositionReinstatedRecord, putEmployeeModificationByRecordId } from '@/api/operation';
+import { SelectValues, BasicData } from '@/interface';
+import { getEmployeePositionReinstatedRecord, putEmployeeModificationByRecordId, getEmployeePositionReinstatedType } from '@/api/operation';
 import moment from 'moment';
 import { message } from 'ant-design-vue';
 import _ from 'lodash';
@@ -52,6 +71,11 @@ interface FormData {
     employeeId: string;
     effectiveDate: string;
     reason: string;
+    orderNum: string;
+    positionName: string;
+    positionId: string;
+    employeePositionChangeTypeName: string;
+    isEdit: boolean;
 }
 @Component({
     name: 's-rehab',
@@ -63,6 +87,7 @@ export default class Rehab extends Vue {
     private $form: any;
     private dateFormat = 'YYYY-MM-DD';
     private rehabPositionOption: SelectValues[] = [{key: '', label: ''}];
+    private typeOption: SelectValues[] = [{key: '', label: ''}];
     private formItemLayout = {
         labelCol: { xs: {span: 24}, sm: {span: 6}},
         wrapperCol: { xs: {span: 24}, sm: {span: 18}},
@@ -75,8 +100,20 @@ export default class Rehab extends Vue {
         labelCol: { xs: {span: 24}, sm: {span: 3}},
         wrapperCol: { xs: {span: 24}, sm: {span: 20}},
     };
+    private transformSelectData(data: any) {
+        return _.map(data, (item: BasicData) => {
+            return {
+                key: item.id,
+                label: item.name,
+            };
+        });
+    }
     private created() {
         this.form = this.$form.createForm(this);
+        getEmployeePositionReinstatedType().then((res: any) => {
+            const data = res.data;
+            this.typeOption = this.transformSelectData(data);
+        });
         getEmployeePositionReinstatedRecord(this.data.employeeId, this.data.id).then((res) => {
             const data = res.data;
             this.rehabPositionOption = _.map(data, (item) => {
@@ -95,8 +132,10 @@ export default class Rehab extends Vue {
         this.form.validateFields((err: any, values: any) => {
             if (!err) {
                 putEmployeeModificationByRecordId(this.data.employeeId, this.data.id, {
-                    reinstatedPositionId: values.reinstatedPositionId,
+                    EmployeePositionChangeTypeId: values.employeePositionChangeTypeId,
+                    orginalPositionId: values.reinstatedPositionId,
                     effectiveDate: moment(values.effectiveDate).format(this.dateFormat),
+                    reason: values.reason,
                 }).then(() => {
                     message.success('更新成功');
                     callback(true);
