f813d63b4e6f4bccf4419131c0071b80983bb0d2
ljl@nssp.biz <ljl@nssp.biz>
2019-12-04 16:20:01 +0800

    添加有薪假 Table，暂时还没对接后台

--- src/views/attendance/salaryRecord/index.vue
+++ src/views/attendance/salaryRecord/index.vue
@@ -1,23 +1,119 @@
 <template>
     <div class="wrapper">
         <div class="staff-head">
-
+            <a-row :gutter="24">
+                <a-form :form="form">
+                    <a-col :lg="8" :md="12" :sm="24">
+                        <a-form-item label="输入员工姓名或工号" v-bind="formItemLayout">
+                            <a-auto-complete placeholder="请输入姓名或工号进行智能搜索"
+                            @search="handleChange" @select="onSelect" @focus="focusHandle" v-decorator="['searchKey']">
+                                <template slot="dataSource">
+                                    <a-select-option v-for="item in employeeDataList" :key="item.value">{{item.text}}</a-select-option>
+                                </template>
+                                <a-input @change="e => valueChange(e.target.value)"></a-input>
+                            </a-auto-complete>
+                        </a-form-item>
+                    </a-col>
+                    <a-col :lg="6" :md="12" :sm="24">
+                        <a-form-item label="指定查询日期" v-bind="formItemLayout">
+                            <a-date-picker v-decorator="['WorkOrderNumber']"></a-date-picker>
+                        </a-form-item>
+                    </a-col>
+                    <a-col :lg="10" :md="12" :sm="24">
+                        <a-form-item>
+                            <a-button type="primary">快速查询</a-button>
+                            <a-button type="primary" style="marginLeft: 10px">查询所有员工有薪假</a-button>
+                        </a-form-item>
+                    </a-col>
+                </a-form>
+            </a-row>
+            <a-operation-recordTable :tabList="tabData" :loading="searchLoading" :paginationData="pagination" @tableChange="pageChange"></a-operation-recordTable>
         </div>
     </div>
 </template>
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
+import { searchEmployeeData } from '@/api/staff';
+import OperationRecordTable from './recordTable.vue';
+import { Pagination } from '@/interface';
+import _ from 'lodash';
+interface EmployeeData {
+    value: string;
+    text: string;
+    id: string;
+}
 @Component({
-    components: {},
+    components: {
+        'a-operation-recordTable': OperationRecordTable,
+    },
     name: 'salaryRecord',
 })
 export default class SalaryRecord extends Vue {
     private form: any;
     private $form: any;
+    private formItemLayout = {
+        labelCol: { xs: {span: 24}, sm: {span: 10}},
+        wrapperCol: { xs: {span: 24}, sm: {span: 14}},
+    };
+    private searchKey: string = '';
+    private searchLoading: boolean = false;
+    private tabData: any = [];
+    private pagination: Pagination = {
+        pageSize: 0,
+        total: 0,
+        current: 0,
+        // onChange: this.pageChange,
+        pageSizeOptions: ['5', '10', '15'],
+        showSizeChanger: true,
+        showTotal: this.showTotal,
+    };
+    private employeeDataList: EmployeeData[] = [];
     private created() {
         this.form = this.$form.createForm(this);
-        
+        this.fetch('');
+    }
+    private pageChange(current: number, pageSize: number) {
+        // this.param.set('PageNumber', current.toString());
+        // this.param.set('PageSize', pageSize.toString());
+        // this.loadData(this.param);
+    }
+    private showTotal(total: string, range: any) {
+        return  `总记录数: ${total}条`;
+    }
+    private handleChange(value: string) {
+        this.fetch(value);
+    }
+    private focusHandle() {
+        this.handleChange(this.searchKey);
+    }
+    private fetch(value: string) {
+        const params = new URLSearchParams();
+        params.set('SearchQuery', value);
+        params.set('ShapedFields', 'fullName,employeeStringId,id');
+        this.changeDataToParamas(params, this.form.getFieldValue('IsIncludeTerminated'), 'FilterProperties.IsIncludeTerminated');
+        searchEmployeeData(params.toString()).then((res) => {
+            const data = res.data;
+            this.employeeDataList = _.map(data, (item) => {
+                return {
+                    value: item.id,
+                    text: item.employeeStringID + '-' + item.fullName,
+                    id: item.employeeStringID,
+                    name: item.fullName,
+                };
+            });
+        });
+    }
+    private changeDataToParamas(params: URLSearchParams, data: boolean, paramName: string) {
+        if (data) {
+            params.set(paramName, data.toString());
+        }
+    }
+    private onSelect(value: string) {
+        const item = _.find(this.employeeDataList, {value});
+        if (item) {
+            this.searchKey = item.value;
+        }
     }
 }
 </script>
---
+++ src/views/attendance/salaryRecord/recordTable.vue
@@ -0,0 +1,128 @@
+<template>
+    <div>
+        <a-table :columns="column" bordered size="small" :dataSource="data" 
+        :loading="tloading" :pagination="pagination" @change="tableChange">
+            <template slot="Index" slot-scope="text,record, index">
+                <span>{{ index + 1 }}</span>
+            </template>
+        </a-table>
+    </div>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import { Emit, Prop, Watch } from 'vue-property-decorator';
+import { ColumnList, Pagination } from '@/interface';
+
+interface TableData {
+    key: string;
+    orderNum: string;
+    num: string;
+    name: string;
+    position: string;
+    typeName: string;
+    type: string;
+    startDateTime: string;
+    endedDateTime: string;
+    isWithSalary: string;
+    totalHours: string;
+    operator: string;
+    operateTime: string;
+}
+@Component({
+    components: {},
+})
+export default class RecordTable extends Vue {
+    @Prop({ default: false }) private loading!: boolean;
+    @Prop() private tabList!: TableData[];
+    @Prop() private paginationData!: Pagination;
+    private tloading: boolean = this.loading;
+    private data: TableData[] = this.tabList;
+    private pagination: Pagination = this.paginationData;
+    private column: ColumnList[] = [{
+        title: '序号',
+        width: 44,
+        dataIndex: 'Index',
+        align: 'center',
+        scopedSlots: { customRender: 'Index' },
+    }, {
+        title: '员工工号',
+        dataIndex: 'num',
+        align: 'center',
+        scopedSlots: { customRender: 'num' },
+    }, {
+        title: '员工姓名',
+        dataIndex: 'name',
+        align: 'center',
+        scopedSlots: { customRender: 'name' },
+    }, {
+        title: '入职日期',
+        dataIndex: 'employeeDate',
+        align: 'center',
+        scopedSlots: { customRender: 'employeeDate' },
+    }, {
+        title: '本年年资可获有薪假h',
+        dataIndex: 'typeName',
+        align: 'center',
+        scopedSlots: { customRender: 'typeName' },
+    }, {
+        title: '往年年资剩余有薪假h',
+        dataIndex: 'type',
+        align: 'center',
+        scopedSlots: { customRender: 'type' },
+    }, {
+        title: '本年已获得有薪假h',
+        dataIndex: 'isWithSalary',
+        align: 'center',
+        scopedSlots: { customRender: 'isWithSalary' },
+    }, {
+        title: '本年已使用有薪假h',
+        dataIndex: 'totalHours',
+        align: 'center',
+        scopedSlots: { customRender: 'totalHours' },
+    }, {
+        title: '冻结已获得有薪假h',
+        dataIndex: 'startDateTime',
+        align: 'center',
+        scopedSlots: { customRender: 'startDateTime' },
+    }, {
+        title: '冻结可预支有薪假h',
+        dataIndex: 'endedDateTime',
+        align: 'center',
+        scopedSlots: { customRender: 'endedDateTime' },
+    }, {
+        title: '至指定查询日期可预支有薪假',
+        dataIndex: 'status',
+        align: 'center',
+        scopedSlots: { customRender: 'status' },
+    }, {
+        title: '本年可用有薪假h',
+        dataIndex: 'operator',
+        align: 'center',
+        scopedSlots: { customRender: 'operator' },
+    }, {
+        title: '指定查询日期',
+        dataIndex: 'createDateTime',
+        align: 'center',
+        scopedSlots: { customRender: 'createDateTime' },
+    }];
+    @Watch('loading')
+    private loadingChange(value: boolean) {
+        this.tloading = value;
+    }
+    @Watch('tabList')
+    private tableDataChange(value: any) {
+        this.data = value;
+    }
+    @Watch('paginationData')
+    private paginationDataChange(value: any) {
+        this.pagination = value;
+    }
+    private tableChange(pagination: any, filters: any, sorter: any) {
+        const pageSize = pagination.pageSize;
+        const pageNum = pagination.current;
+        this.$emit('tableChange', pageNum, pageSize);
+    }
+}
+</script>
+
